af93699ba1999634f47f5bedf4d24685fd7f33dc
Aaron Mangan <azza.mangan@gmail.com>
2025-03-18 09:02:32 +1000

    feature/transmittal - updated app logo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -6,7 +6,7 @@ export default function GuestLayout({ children }) {
         <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 text-gray-500 fill-current" />
+                    <ApplicationLogo className="w-20 h-20 text-gray-500 fill- current" />
                 </Link>
             </div>
 
